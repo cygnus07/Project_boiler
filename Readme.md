8e5cd023e680e12033ac53ead6b5f23c70ea24aa
cygnus07 <kuldeep2111051@agkec.ac.in>
2024-04-07 18:41:10 +0530

    defined models and played with userSchema

--- Readme.md
+++ Readme.md
@@ -1,3 +1,8 @@
 Project boiler for professional project structure
 
 1. Use gitignore generators to generate the gitignore file content
+
+2. PACKAGES
+    -- dotenv
+    -- express
+    -- mongoose
---
+++ src/models/video.model.js
@@ -0,0 +1,48 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+            
+        },
+        thumbnail: {
+            type: String,
+            required: true,
+            
+        },
+        description: {
+            type: String,
+            required: true,
+           
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        duration: {
+            type: Number, //cloudinary duration
+            required: true,
+            trim: true,
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    },
+ {timestamps: true})
+
+
+ videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema)
