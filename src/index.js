30d31172113ee3d85efa8545e20d8988224e2bdf
cygnus07 <kuldeep2111051@agkec.ac.in>
2024-04-08 09:43:12 +0530

    defined middlewares and controllers and create register route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 
 dotenv.config({path: './.env'});
@@ -15,7 +16,7 @@ connectDB()
     })
 })
 .catch((error) => {
-    console.log("MongDB connection failed !!", err)
+    console.log("MongDB connection failed !!", error)
 })
 
 
