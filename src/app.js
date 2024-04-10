fdb768347d64a4b2c5d22fd8af12ef3ddef88bbb
cygnus07 <kuldeep2111051@agkec.ac.in>
2024-04-10 19:25:57 +0530

    added ballu's code as comment in app.js file to find the local ip address

--- src/app.js
+++ src/app.js
@@ -22,8 +22,38 @@ import userRouter from "./routes/user.routes.js";
 app.use("/api/v1/users", userRouter);
 
 
+  // ballu's code to get local ip address
+// app.get("/", (req,res) => {
+//     res.download(`src/2160.mp4`)
+// })
+
+
+
 
 // http://localhost:3000/api/v1/users/register
+
+
+  // ballu's code to get local ip address
+// import os from "os";
+// const getLocalIPv4Address = () => {
+//   const ifaces = os.networkInterfaces();
+
+//   for (const iface in ifaces) {
+//     for (const details of ifaces[iface]) {
+//       if (details.family === 'IPv4' && !details.internal) {
+//         return details.address;
+//       }
+//     }
+//   }
+// };
+// const server = app.listen(process.env.PORT, function () {
+//   console.log('Server is up');
+//   const host = getLocalIPv4Address();
+//   console.log(`Server is running at http://${host}:${server.address().port}`);
+//  // generateSitemap();
+// });
+
+
 
 
 export {app}
